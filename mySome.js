192916a419e8b578f231711d7c4b292155a3547a
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-03-02 13:08:42 -0500

    Updated file

--- mySome.js
+++ mySome.js
@@ -1,4 +1,19 @@
 // SOME //
-Array.prototype.mySome = function() {
-    // Place your code here.
+Array.prototype.mySome = function(callbackFn) {
+    for (let i = 0; i < this.length; i++) {
+        if (callbackFn(this[i])) {
+            return true;
+        }
+    }
+    return false;
 };
+
+
+// TEST //
+let myArray = [1,2,3,4,5];
+
+let even = (element) => element % 2 === 0;
+console.log("SOME");
+console.log(myArray.some(even));
+console.log("mySOME");
+console.log(myArray.mySome(even));
