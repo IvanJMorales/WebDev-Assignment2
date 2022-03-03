1f5068e535b32afa092554bd3aa03abba5b0a346
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-03-03 17:26:01 -0500

    Updated file

--- myReduce.js
+++ myReduce.js
@@ -1,4 +1,19 @@
 // REDUCE //
 Array.prototype.myReduce = function() {
-    // Place your code here.
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+            const combine = this[i] + this[j];
+        }
+    }
 };
+
+// TEST //
+const myArray = [1,2,3,4,5];
+
+const sum = myArray.reduce((previousValue, currentValue) => previousValue + currentValue);
+console.log("MY REDUCE");
+console.log(sum);
+
+const sumWithInitial = myArray.reduce((previousValue, currentValue) => previousValue + currentValue);
+console.log("REDUCE");
+console.log(sumWithInitial);
