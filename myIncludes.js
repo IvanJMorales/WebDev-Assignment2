72f920b32f34fd57c6f36b689ea25e319291b769
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-03-03 17:59:09 -0500

    Updated file

--- myIncludes.js
+++ myIncludes.js
@@ -1,4 +1,20 @@
 // INCLUDES //
-Array.prototype.myIncludes = function() {
-    // Place your code here.
+Array.prototype.myIncludes = function(callbackFn) {
+    for(let i = 0; i < this.length; i++) {
+        if (this[i] === callbackFn) {
+            return true;
+        }
+    }
+    return false;
 };
+
+// TEST //
+const myArray = [1,2,3,4,5];
+
+// MY INCLUDES //
+console.log("MY INCLUDES");
+console.log(myArray.myIncludes(2));
+
+// INCLUDES //
+console.log("INCLUDES");
+console.log(myArray.includes(2));
