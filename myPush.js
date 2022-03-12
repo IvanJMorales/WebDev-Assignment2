62f79962b03e7697be2f148a5d52e95d580d9653
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-03-11 19:25:42 -0500

    Updated file

--- myPush.js
+++ myPush.js
@@ -1,4 +1,20 @@
 // PUSH //
-Array.prototype.myPush = function() {
-    // Place your code here.
+Array.prototype.myPush = function(callbackFN) {
+    this[this.length] = callbackFN;
 };
+
+// TEST //
+const myArray = [1,2,3,4,5];
+
+// MY PUSH //
+console.log("MY PUSH");
+myArray.myPush(6);
+console.log(myArray);
+
+// REMOVE MY PUSH APPEND //
+myArray.pop(6);
+
+// PUSH //
+console.log("PUSH");
+myArray.push(6);
+console.log(myArray);
