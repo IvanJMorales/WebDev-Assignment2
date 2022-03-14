118fc68108c965dc69d78c615c3c5b68a4a45ace
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-03-14 12:17:13 -0400

    Updated file

--- myLastIndexOf.js
+++ myLastIndexOf.js
@@ -1,4 +1,20 @@
 // LASTINDEXOF //
-Array.prototype.myLastIndexOf = function() {
-    // Place your code here.
+Array.prototype.myLastIndexOf = function(callbackFN) {
+    for (let i = this.length - 1; i >= 0; i--) {
+        if (this[i] === callbackFN) {
+            return i;
+        }
+    }
+    return -1;
 };
+
+// TEST //
+const myArray = [1,2,3,4,5];
+
+// MY LAST INDEX OF //
+console.log("MY LAST INDEX OF");
+console.log(myArray.myLastIndexOf(2));
+
+// LAST INDEX OF //
+console.log("LAST INDEX OF");
+console.log(myArray.lastIndexOf(2));
