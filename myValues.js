2aa0d2bbf1ccffc77dfb8b38d575503063774f5f
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-03-15 16:42:52 -0400

    Updated file

--- myValues.js
+++ myValues.js
@@ -1,4 +1,23 @@
-// VALUES //
-Object.myValues = function() {
-    // Place your code here.
+// KEYS //
+Object.myValues = function(callbackFN) {
+    let array = [];
+    for (const value in callbackFN) {
+        array.push(callbackFN[value]);
+    }
+    return array;
 };
+
+// TEST //
+const object1 = {
+    a: "Hello",
+    b: 42,
+    c: false
+};
+
+// MY KEYS //
+console.log("MY VALUES");
+console.log(Object.myValues(object1));
+
+// KEYS //
+console.log("VALUES");
+console.log(Object.values(object1));
